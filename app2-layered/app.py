cc87eaa54a034590a0602602b0fb9509d4efa016
The-Biggest-Bug <smithz3@etsu.edu>
2026-07-02 14:15:36 -0400

    Added import os and other line

--- app2-layered/app.py
+++ app2-layered/app.py
@@ -207,3 +207,4 @@ if __name__ == "__main__":
     print("    strategies_and_observers.py — patterns\n")
     import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    #comment
